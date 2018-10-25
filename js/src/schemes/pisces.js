d1ce9baa4241e57324eb95d260968e4b9caae749
Wenhao.Wu <wuwenhao0327@gmail.com>
2018-10-25 16:27:31 +0800

    Site updated:2018-10-25 16:27:25

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 25 2018 16:23:13 GMT+0800 (中国标准时间)
+// build time:Thu Oct 25 2018 16:26:39 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
